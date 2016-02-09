77c1df895c893ed3b61ed130f31a70975b38d657
Tyokopai <take353@gmail.com>
2016-02-09 13:12:58 +0900

    index form

--- oic-rental/resources/views/welcome.blade.php
+++ oic-rental/resources/views/welcome.blade.php
@@ -1,45 +1,42 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+<head>
+  <title>貸し出し室テム</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <header>
+      <nav class="navbar navbar-inverse navbar-static-top navbar-fixed">
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">貸し出しシステム</a>
             </div>
         </div>
-    </body>
+    </nav>
+  </header>
+  <div class="container" style="margin-top:100px; padding:20px 0;">
+    <h1 class="text-center">登録フォーム</h1>
+    <form class="form-horizontal col-md-4 col-md-offset-4" action="" method="POST">
+      <div class="form-group">
+        <label class="control-label" for="student-number">学籍番号</label>
+        <div >
+          <input type="text" id="student-number" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label" for="loan-number">貸し出し番号</label>
+        <div>
+          <input type="password" id="loan-number" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-md-offset-4 col-md-4">
+          <button type="submit" class="btn btn-primary btn-block">登録</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
+</body>
 </html>
