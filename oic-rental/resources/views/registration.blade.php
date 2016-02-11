52fc1d4306d2770584c9f8259027151dd87756c1
hideyasu <b4142@oic.jp>
2016-02-12 04:56:06 +0900

    database sql

--- oic-rental/resources/views/registration.blade.php
+++ oic-rental/resources/views/registration.blade.php
@@ -17,26 +17,9 @@
     </nav>
   </header>
   <div class="container" style="margin-top:100px; padding:20px 0;">
-    <h1 class="text-center">登録フォーム</h1>
-    <form class="form-horizontal col-md-4 col-md-offset-4" action="/registration" method="POST">
-      <div class="form-group">
-        <label class="control-label" for="student-number">学籍番号</label>
-        <div >
-          <input type="text" id="student-number" name="student-number" class="form-control">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="control-label" for="loan-number">貸し出し番号</label>
-        <div>
-          <input type="password" id="loan-number" name="loan-number" class="form-control">
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-md-offset-4 col-md-4">
-          <button type="submit" class="btn btn-primary btn-block">登録</button>
-        </div>
-      </div>
-    </form>
+    <h1 class="text-center">rental registration</h1>
+    Hello!! <?= $student_name ?>.<br>
+    You rented <?= $item_name ?>. 
   </div>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
 </body>
