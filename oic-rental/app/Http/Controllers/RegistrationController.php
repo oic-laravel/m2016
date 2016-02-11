046e77467b8e597929d961a67b40a664fda08651
hideyasu <b4142@oic.jp>
2016-02-12 05:37:27 +0900

    sql plus

--- oic-rental/app/Http/Controllers/RegistrationController.php
+++ oic-rental/app/Http/Controllers/RegistrationController.php
@@ -15,16 +15,13 @@ class RegistrationController extends Controller
     public function index()
 	{
 		$data = [];
-        $data["student_name"] = "Luke";
-        $data["item_name"] = "Skywalker";
- 		
+
  		$student = DB::table('student')->where('student_number', 'b4142')->first();
-
- 		$data["student_name"] = $student->student_name;
-
  		$item = DB::table('item')->where('item_id', '1')->first();
+ 		$data["student_name"] = $student->student_name;
  		$data["item_name"] = $item->item_name;
         // view関数の第２引数に配列を渡す
-        return view('registration', $data);
+    
+    	return view('registration', $data);
 	}
 }
